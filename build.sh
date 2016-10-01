805c00d69778018426b4dec6bda16cc5057a5499
Matt Gale <matt@littleball.co.uk>
2016-10-01 12:18:34 +0100

    Added basic screen width update and render

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ case $1 in
   build)
     mkdir -p dist
     $MOD/pug < src/index.pug > dist/index.html
-    $MOD/rollup -c
+    $MOD/browserify src/app.js > dist/x.js
     cp src/*.png dist
     ;;
   watch)
-    $MOD/nodemon -e pug --ignore temp -x "$MOD/pug < src/index.pug > dist/index.html" & \
-      $MOD/nodemon -e js --ignore temp -x "$MOD/rollup -c"
+    $MOD/nodemon -e pug --ignore dist -x "$MOD/pug < src/index.pug > dist/index.html" & \
+      $MOD/nodemon -e js --ignore dist -x "$MOD/browserify src/app.js > dist/x.js"
     ;;
   *)
     echo "usage: build [build|watch]"
--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+const screenFactory = require('lb-screen-factory')
+const loop = require('lb-loop')
+
+let game
+
+const screen = screenFactory(window.document.querySelectorAll('canvas')[0])
+
+screen.setSize({
+  width: 160,
+  height: 144
+})
+
 const assets = [
   {
     name: 'textures',
@@ -7,8 +19,24 @@ const assets = [
   }
 ]
 
+const update = () => {}
+const render = () => {
+  const ctx = screen.getContext()
+  ctx.fillStyle = `hsl(180, 50%, ${Math.floor(Math.random() * 10) + 45}%)`
+  ctx.fillRect(0, 0, screen.getWidth(), screen.getHeight())
+
+  ctx.fillStyle = '#000'
+  ctx.fillRect(screen.getWidth() * 0.5, screen.getHeight() * 0.5, 1, 1)
+}
+
+game = loop({update, render})
+
 const renderStartScreen = () => {
-  window.document.write('All assets loaded')
+  const ctx = screen.getContext()
+  ctx.fillStyle = 'hsl(180, 50%, 50%)'
+  ctx.fillRect(0, 0, screen.getWidth(), screen.getHeight())
+
+  game.start()
 }
 
 const hasResourcesLoaded = () => {
